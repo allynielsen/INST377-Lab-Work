626aa2e97c8ae5ccd40c18313f16665d2c40a6bb
allynielsen <86061526+allynielsen@users.noreply.github.com>
2021-10-25 15:42:15 -0400

    lab5

---
+++ client/lab_5/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const submitButton = document.querySelector('.submitButton');
+
+  submitButton.addEventListener('click', () => {
+    console.log('clicked button yay');
+  });
+});
